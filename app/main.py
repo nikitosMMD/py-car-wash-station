f2eae72b05f6c2ffa21f4071312a1ea9bc0bcd2b
pronikito38@gmail.com <pronikito38@gmail.com>
2025-04-27 21:51:37 +0300

    Решение

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str,
+    ) -> None:
         self.comfort_class = max(1, min(7, comfort_class))
         self.clean_mark = max(1, min(10, clean_mark))
         self.brand = brand
@@ -13,7 +18,9 @@ class CarWashStation:
         average_rating: float,
         count_of_ratings: int,
     ) -> None:
-        self.distance_from_city_center = max(1.0, min(11.0, distance_from_city_center))
+        self.distance_from_city_center = max(
+            1.0, min(11.0, distance_from_city_center)
+        )
         self.clean_power = max(1.0, min(10.0, clean_power))
         self.average_rating = round(max(1.0, min(5.0, average_rating)), 1)
         self.count_of_ratings = max(0, count_of_ratings)
@@ -47,4 +54,6 @@ class CarWashStation:
     def rate_service(self, rate: float) -> None:
         total_rating = self.average_rating * self.count_of_ratings + rate
         self.count_of_ratings += 1
-        self.average_rating = round(total_rating / self.count_of_ratings, 1)
+        self.average_rating = round(
+            total_rating / self.count_of_ratings, 1
+        )
